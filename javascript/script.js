bc0997987357e083ae032492cd92e52f78df368f
Thomas <brelotthomas@gmail.com>
2024-10-01 21:29:23 +0200

    modification du site

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.getElementById('hamburger');
-  const navLinks = document.getElementById('nav-links');
+// Sélectionner tous les liens de navigation
+const navLinks = document.querySelectorAll('.nav-link');
 
-  if (hamburger && navLinks) {
-    hamburger.addEventListener('click', function () {
-      navLinks.classList.toggle('open');
-      hamburger.classList.toggle('open');
-    });
-  } else {
-    console.error('Hamburger button or navigation links not found!');
-  }
+// Ajouter un événement de défilement pour gérer l'état actif des liens
+window.addEventListener('scroll', () => {
+  let fromTop = window.scrollY;
+
+  navLinks.forEach((link) => {
+    let section = document.querySelector(link.getAttribute('href'));
+
+    if (
+      section.offsetTop <= fromTop + 50 &&
+      section.offsetTop + section.offsetHeight > fromTop + 50
+    ) {
+      navLinks.forEach((link) => {
+        link.classList.remove('active');
+      });
+      link.classList.add('active');
+    }
+  });
 });
